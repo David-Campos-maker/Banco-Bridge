358c3679d5e57224f5ae21ca6d71320ee16b2221
David-Campos-maker <davidcamposcruz2016@gmail.com>
2023-06-03 20:15:44 -0300

    Login

--- Client/helpers/menu_functions.py
+++ Client/helpers/menu_functions.py
@@ -1,76 +1,78 @@
 import socket
 
 def main_menu(client_socket: socket.socket):
-    while True:
-        print("******************")
-        print("**  Main Menu   **")
-        print("******************\n")
-        
-        print("1. Option 1")
-        print("2. Option 2")
-        print("3. Option 3")
-        print("4. Logout")
+    print("******************")
+    print("**  Main Menu   **")
+    print("******************\n")
     
-        choice = input("Choose the option that indicates what you want to do -> ")
+    print("1. Option 1")
+    print("2. Option 2")
+    print("3. Option 3")
+    print("4. Logout")
+
+    choice = input("Choose the option that indicates what you want to do -> ")
+
+    if choice == "4":
+        return
+
+    client_socket.send(f"main:{choice}".encode())
     
-        if choice == "4":
-            return
+    data = client_socket.recv(1024).decode()
+    print(data)
     
-        client_socket.send(f"main:{choice}".encode())
-        
-        data = client_socket.recv(1024).decode()
-        print(data)
+    main_menu(client_socket)
 
 def login_or_sign_in_menu(client_socket: socket.socket):
-    while True:
-        print("******************")
-        print("**     Menu     **")
-        print("******************\n")
+    print("******************")
+    print("**     Menu     **")
+    print("******************\n")
+    
+    print("1. Sign in")
+    print("2. Login")
+    print("3. Exit")
+
+    choice = input("Choose the option that indicates what you want to do -> ")
+
+    if choice == "1":
+        client_socket.send("login:1".encode())
+        name = input('Your name -> ')
+        phone = input('Your phone number (Use your location code ex: +55-your number) -> ')
+        card_password =  input('Choose a password for your card. It must be numeric and contain 6 digits -> ')
         
-        print("1. Sign in")
-        print("2. Login")
-        print("3. Exit")
-    
-        choice = input("Choose the option that indicates what you want to do -> ")
-    
-        if choice == "1":
-            client_socket.send("login:1".encode())
-            name = input('Your name -> ')
-            phone = input('Your phone number (Use your location code ex: +55-your number) -> ')
-            card_password =  input('Choose a password for your card. It must be numeric and contain 6 digits -> ')
+        user_info = ",".join([name, phone, card_password])
+        client_socket.send(user_info.encode())
+
+        data = client_socket.recv(1024).decode()
+
+        while data == "Enter access password":
+            access_password = input('Choose an access password. It must contain 8 characters, including letters and special symbols -> ')
+            client_socket.send(access_password.encode())
             
-            user_info = ",".join([name, phone, card_password])
-            client_socket.send(user_info.encode())
-    
             data = client_socket.recv(1024).decode()
-    
-            while data == "Enter access password":
-                access_password = input('Choose an access password. It must contain 8 characters, including letters and special symbols -> ')
-                client_socket.send(access_password.encode())
-                
-                data = client_socket.recv(1024).decode()
-    
+
+        print(data)
+        
+    elif choice == "2":
+        uid = input("Enter your account uid -> ")
+        password = input("Enter your password -> ")
+        
+        client_socket.send("login:2".encode())
+        client_socket.send(uid.encode())
+        client_socket.send(password.encode())
+
+        data = client_socket.recv(1024).decode()
+
+        if "Hello" in data:
             print(data)
-            
-        elif choice == "2":
-            uid = input("Enter your account uid -> ")
-            password = input("Enter your password -> ")
-            
-            client_socket.send("login:2".encode())
-            client_socket.send(uid.encode())
-            client_socket.send(password.encode())
-    
-            data = client_socket.recv(1024).decode()
-    
-            if "Hello" in data:
-                print(data)
-                main_menu(client_socket)
-            else:
-                print(data)
-        
+            main_menu(client_socket)
         else:
-            client_socket.send("login:3".encode())
-            exit_message = client_socket.recv(1024).decode()
-            
-            print(exit_message)
-            return
+            print(data)
+    
+    else:
+        client_socket.send("login:3".encode())
+        exit_message = client_socket.recv(1024).decode()
+        
+        print(exit_message)
+        return
+    
+    login_or_sign_in_menu(client_socket)
--- Server/functions/utils/client_helpers.py
+++ Server/functions/utils/client_helpers.py
@@ -1,95 +1,67 @@
 import socket
-import time
 from functions.login import login
 from functions.sign_in import sign_in , get_access_password
 from functions.utils.account_helpers import get_account_by_uid
 
-# Global variables to store failed login attempts information
-last_login_attempt_time = 0
-failed_login_attempts = 0
-
 def handle_main_menu(client_socket: socket.socket):
-    while True:
-        # Receive message from client
-        message = client_socket.recv(1024).decode()
+    # Receive message from client
+    message = client_socket.recv(1024).decode()
+    
+    # Handle main menu choice
+    if message == "main:1":
+        # Handle option 1
+        client_socket.send("Option 1 executed".encode())
+            
+    elif message == "main:2":
+        # Handle option 2
+        client_socket.send("Option 2 executed".encode())
         
-        # Handle main menu choice
-        if message == "main:1":
-            # Handle option 1
-            client_socket.send("Option 1 executed".encode())
-                
-        elif message == "main:2":
-            # Handle option 2
-            client_socket.send("Option 2 executed".encode())
-            
-        elif message == "main:3":
-            # Handle option 3
-            client_socket.send("Option 3 executed".encode())
-            
-        elif message == "main:4":
-            # Handle logout
-            return
+    elif message == "main:3":
+        # Handle option 3
+        client_socket.send("Option 3 executed".encode())
+        
+    elif message == "main:4":
+        # Handle logout
+        return
+    
+    handle_main_menu(client_socket)
 
 def handle_client(client_socket: socket.socket):
-    global last_login_attempt_time
-    global failed_login_attempts
+    # Receive message from client
+    message = client_socket.recv(1024).decode()
     
-    while True:
-        # Receive message from client
-        message = client_socket.recv(1024).decode()
+    # Handle login or sign in choice
+    if message == "login:1":
+        # Handle sign in
+        user_info = client_socket.recv(1024).decode()
+        name, phone, card_password = user_info.split(",")
         
-        # Handle login or sign in choice
-        if message == "login:1":
-            # Handle sign in
-            user_info = client_socket.recv(1024).decode()
-            name, phone, card_password = user_info.split(",")
+        access_password = get_access_password(client_socket)
+        
+        if access_password is not None:
+            sign_in(name, phone, access_password, int(card_password))
+            client_socket.send("Sign in successful".encode())
             
-            access_password = get_access_password(client_socket)
+    elif message == "login:2":
+        # Handle login
+        uid = client_socket.recv(1024).decode()
+        password = client_socket.recv(1024).decode()
+        
+        if login(uid, password):
+            account = get_account_by_uid(uid)
             
-            if access_password is not None:
-                sign_in(name, phone, access_password, int(card_password))
-                client_socket.send("Sign in successful".encode())
-                
-        elif message == "login:2":
-            # Check if login should be blocked
-            current_time = time.time()
-            if failed_login_attempts >= 3 and current_time - last_login_attempt_time < 60:
-                client_socket.send("Too many failed login attempts. Please wait 1 minute before trying again.".encode())
-                continue
-            
-            # Handle login
-            uid = client_socket.recv(1024).decode()
-            password = client_socket.recv(1024).decode()
-            
-            if login(uid, password):
-                account = get_account_by_uid(uid)
-                
-                if account is not None:
-                    client_socket.send(f"Hello {account['name']}! \nBalance: {account['balance']}".encode())
-                    handle_main_menu(client_socket)
-                    
-                    # Reset failed login attempts
-                    failed_login_attempts = 0
-                else:
-                    client_socket.send("Account not found!".encode())
-                    
-                    # Increment failed login attempts
-                    failed_login_attempts += 1
-                    
-                    # Update last login attempt time
-                    last_login_attempt_time = current_time      
-                    
+            if account is not None:
+                client_socket.send(f"Hello {account['name']}! \nBalance: {account['balance']}".encode())
+                handle_main_menu(client_socket)
+
             else:
-                client_socket.send("Login failed".encode())
+                client_socket.send("Account not found!".encode())    
                 
-                # Increment failed login attempts
-                failed_login_attempts += 1
-                
-                # Update last login attempt time
-                last_login_attempt_time = current_time
-                
-        elif message == "login:3":
-            client_socket.send("Goodbye!".encode())
-            client_socket.close()
+        else:
+            client_socket.send("Login failed".encode())
             
-            return "Exit"
+    elif message == "login:3":
+        client_socket.send("Goodbye!".encode())
+        return "Exit"
+    
+    handle_client(client_socket)
