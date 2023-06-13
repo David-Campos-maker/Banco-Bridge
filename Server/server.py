52575565e88faf83b13cb54e6cbe5413d326140f
David-Campos-maker <davidcamposcruz2016@gmail.com>
2023-06-13 16:31:26 -0300

    Shutting down server according to resource usage

--- Server/server.py
+++ Server/server.py
@@ -1,4 +1,6 @@
 import socket
+import os
+import psutil
 from functions.utils.client_helpers import handle_client
 
 # Create a TCP/IP socket
@@ -13,7 +15,28 @@ server_socket.listen(1)
 
 running: bool = True
 
+max_memory_usage = 500 * 1024 * 1024 # Sets the maximum memory usage in bytes (500 MB)
+max_cpu_usage = 90 # Sets the maximum CPU usage in percentage
+
 while running:
+    # Gets the current memory usage of the process
+    memory_usage = psutil.Process(os.getpid()).memory_info().rss
+    
+    # Checks if memory usage has exceeded the maximum limit
+    if memory_usage > max_memory_usage:
+        print("Exceeded maximum memory usage, terminating the server")
+        running = False
+        break
+    
+    # Get current CPU usage
+    cpu_usage = psutil.cpu_percent()
+    
+    # Checks if CPU usage has exceeded the maximum threshold
+    if cpu_usage > max_cpu_usage:
+        print("Exceeded maximum CPU usage, terminating the server")
+        running = False
+        break
+
     # Wait for a connection
     print('waiting for a connection')
     client_socket, client_address = server_socket.accept()
@@ -26,11 +49,7 @@ while running:
         client_socket.send(welcome_message.encode())
         
         # Handle client connection
-        result = handle_client(client_socket)
-        
-        if result == "Exit":
-            print(result)
-            running = False
+        handle_client(client_socket)
         
     finally:
         # Clean up the connection
